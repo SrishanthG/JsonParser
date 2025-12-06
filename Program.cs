5a565b352265f473b7453b84ca3a971ab9164e00
SrishanthG <srishanthg1@gmail.com>
2025-12-06 10:37:14 -0500

    Mulitple lines tested

--- Program.cs
+++ Program.cs
@@ -5,10 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Parser parserInvalid = new Parser(@"C:\Users\srish\Downloads\tests\step1\invalid.json");
-
         Console.WriteLine("Invalid JSON Test: " + parserInvalid.IsValidJson());
 
         Parser parserValid = new Parser(@"C:\Users\srish\Downloads\tests\step1\valid.json");
         Console.WriteLine("Valid JSON Test: " + parserValid.IsValidJson());
+
+        Parser parserMultiLine = new Parser(@"C:\Users\srish\Downloads\tests\step1\validMultiline.json");
+        Console.WriteLine("Valid Multiline JSON Test: " + parserMultiLine.IsValidJson());
     }
 }
